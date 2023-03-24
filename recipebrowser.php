e35e72d13de70eb17a6145caab85bedd9480f867
Michael Galambos <90899444+yibist@users.noreply.github.com>
2023-03-24 08:30:37 +0100

    adjust the api arguments arrays

--- recipebrowser.php
+++ recipebrowser.php
@@ -20,18 +20,79 @@ class recipebrowser
 }
 class Api
 {
+    //, "health", "cuisineType", "mealType", "dishType", "calories", "glycemicIndex", "excluded"
     public static array $apiargs = array();
-    private static array $apiarglimits = array("diet", "health", "cuisineType", "mealType", "dishType", "calories", "glycemicIndex", "excluded");
-
+    public static array $apiarglimits = array( 'diet' => array(
+                                                    "balanced",
+                                                    "high-protein",
+                                                    "low-carb",
+                                                    "low-fat"),
+                                                'health' => array(
+                                                    "alcohol-cocktail",
+                                                    "alcohol-free",
+                                                    "DASH",
+                                                    "fodmap-free",
+                                                    "immuno-supportive",
+                                                    "keto-friendly",
+                                                    "low-sugar",
+                                                    "Mediterranean",
+                                                    "peanut-free",
+                                                    "pescatarian",
+                                                    "sugar-conscious",
+                                                    "sulfite-free",
+                                                    "tree-nut-free",
+                                                    "vegan",
+                                                    "vegetarian",
+                                                    "wheat-free"),
+                                                'cuisineType' => array(
+                                                    "American",
+                                                    "Asian",
+                                                    "British",
+                                                    "Caribbean",
+                                                    "Central Europe",
+                                                    "Chinese",
+                                                    "Eastern Europe",
+                                                    "French",
+                                                    "Indian",
+                                                    "Italian",
+                                                    "Japanese",
+                                                    "Kosher",
+                                                    "Mediterranean",
+                                                    "Mexican",
+                                                    "Middle Eastern",
+                                                    "Nordic",
+                                                    "South American",
+                                                    "South East Asian"
+                                                ),
+                                                'mealType' => array(
+                                                    "Breakfast",
+                                                    "Dinner",
+                                                    "Lunch",
+                                                    "Snack",
+                                                    "Teatime"),
+                                                'dishType' => array(
+                                                    "Biscuits and cookies",
+                                                    "Bread",
+                                                    "Cereals",
+                                                    "Condiments and sauces",
+                                                    "Desserts",
+                                                    "Drinks",
+                                                    "Main course",
+                                                    "Pancake",
+                                                    "Preps",
+                                                    "Preserve",
+                                                    "Salad",
+                                                    "Sandwiches",
+                                                    "Side dish",
+                                                    "Soup",
+                                                    "Starter",
+                                                    "Sweets"));
     public static function data(string $q, ?array $args)
     {
         $url = "https://edamam-recipe-search.p.rapidapi.com/search?q=$q";
         $append = "";
         if (isset($args)) {
             foreach ($args as $key => $val) {
-                if (!in_array($key, self::$apiarglimits)) {
-                    return 'a wrong api parameter was inserted';
-                }
                 $append .= "&" . $key . "=" . $val;
             }
         }
@@ -56,4 +117,32 @@ class Api
             return self::$apiargs["q=$q$append"];
         }
     }
+}
+echo
+"
+<form action='' method='post'>
+  <input type='submit' name='submit' value='Submit'>
+    <input type='submit' name='submit2' value='Submit2'>
+</form>
+";
+if (isset($_POST['submit'])) {
+    foreach (Api::$apiarglimits as $key => $value) {
+        foreach ($value as $key2 => $value2) {
+            echo $key . " + " . $value2;
+            echo "<br>";
+            $result = Api::data('chicken', array($key => $value2));
+            if (is_string($result)) {
+                echo $result;
+                echo "<br>";
+                echo "<br>";
+            }
+
+        }
+    }
+}
+if (isset($_POST['submit2'])) {
+            $result = Api::data('chicken', array('diet' => 'gluten-free'));
+            if (is_string($result)) {
+                echo $result;
+            }
 }
