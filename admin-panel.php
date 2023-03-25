31424a80d3dd0db049fc6212ef25355a70c0200e
Bojan Rodic <bojan.rodic@stud.edubs.ch>
2023-03-25 14:13:42 +0100

    AdminPanel fixed

--- admin-panel.php
+++ admin-panel.php
@@ -6,16 +6,12 @@ if ( ! defined( 'ABSPATH' ) )
 }
 class Admin
 {
-	public function __construct()
-	{
-		add_action("admin_menu", array($this, "createAdminPanel"));
-	}
-
 	//Attribute für den Admin Panel
-	public $titel = "Admin site for recipe browser";
-	public $titelMenu = "Admin Panel";
-	public $menuSlug = "AdminPanel";
+	public $titel = 'Admin Panel Recipebrowser';
+	public $titelMenu = 'Admin Panel';
+	public $menuSlug = 'AdminPanel';
 
+	//Erstellt den Admin Panel
 	public function createAdminPanel()
 	{
 		add_options_page
@@ -27,35 +23,85 @@ class Admin
 			array($this, 'adminPanelContent')
 		);
 	}
-	public function adminPanelContent()
+	//Inhalt des Admin Panels
+	public function adminPanelContent() : void
 	{
-		echo
-		'
-			<div class="wrap" xmlns="http://www.w3.org/1999/html">
-			<h1>Settings recipie browser plugin</h1>
-			<h2>Theme</h2>
-			<form>
-				<label for="theme">Theme</label><br>
-				<select id="theme">
-					<option value="light">Light</option>
-					<option value="dark">Dark</option>
-				</select><br>
-				<input type="submit" value="apply">
-			</form>
-			</div>
-		';
-	}
+		echo "<form action='options.php' method='post'>";
 
-	private function adminFile()
+		settings_fields('recipebrowser_setting_group');
+		do_settings_sections('recipebrowser_setting_section');
+		submit_button('Apply', 'primary', 'submit', false);
+
+		echo "</form>";
+	}
+	//Funktionen (Inputfelder) für den Admin Panel
+	public function adminPanelSettingFields()
 	{
-		if(file_exists("config.txt"))
+		//Erstellt eine Gruppe
+		add_settings_section
+		(
+			'recipebrowser_setting_group',
+			'RecipeBrowser Settings',
+			array($this, 'sectionCall'), //nutzlose funktion, muss aber funktionieren
+			'recipebrowser_setting_section'
+		);
+		//Sagt das die Fields Setting Felder sind
+		register_setting
+		(
+			'recipebrowser_setting_group',
+			'key',
+			Array('type'=>'string',
+			      'show_in_rest'=>'true',
+			      'default' => 'default')
+		);
+		register_setting
+		(
+			'recipebrowser_setting_group',
+			'theme',
+			Array('type'=>'string',
+			      'show_in_rest'=>'true',
+			      'theme' => 'light')
+		);
+		//Fügt spezifisches Feld zu der Gruppe hinzu
+		add_settings_field
+		(
+			'key',
+			'API Key',
+			array($this, 'keyCall'),
+			'recipebrowser_setting_section',
+			'recipebrowser_setting_group'
+		);
+		add_settings_field
+		(
+			'theme',
+			'Theme',
+			array($this, 'themeCall'),
+			'recipebrowser_setting_section',
+			'recipebrowser_setting_group'
+		);
+	}
+	public function keyCall()
+	{
+		$val = get_option('key');
+		echo "<input id='key' name='key' type='text' value='$val' required />";
+	}
+	public function themeCall()
+	{
+		$val = "<select name='theme' id='theme'>";
+
+		if(get_option("theme") == "light")
 		{
-			$file = fopen("config.txt", "a");
-			fwrite("test");
+			$val .= "<option value='light' selected>Light</option>
+  				<option value='dark'>Dark</option>
+				</select>";
 		}
 		else
 		{
-			//$fwrite
+			$val .= "<option value='light'>Light</option>
+  				<option value='dark' selected>Dark</option>
+				</select>";
 		}
+		echo $val;
 	}
+	public function sectionCall(){}
 }
--- recipebrowser.php
+++ recipebrowser.php
@@ -17,15 +17,17 @@ defined('ABSPATH') or die('no');
 
 class RecipeBrowser {
 	public shortcodeyanni $shortcodeyanni;
-
+	public Admin $adminPanel;
 	function __construct() {
 		require_once("admin-panel.php");
-		$adminPanel = new Admin();
+		$this->adminPanel = new Admin();
 		include_once 'shortcodeyanni.php';
 		$this->shortcodeyanni = new shortcodeyanni();
 	}
 
 	function register() {
+		add_action('admin_init', array($this->adminPanel, 'adminPanelSettingFields'));
+		add_action("admin_menu", array($this->adminPanel, "createAdminPanel"));
 		add_shortcode( 'testShortcode', array( $this->shortcodeyanni, 'testShortcode' ) );
 	}
 }
